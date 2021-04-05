39585a372552accaf9fc578d97e9b13ac0b691d0
ElenaFaramazova <elena.faramazova@mail.ru>
2021-04-05 15:28:06 +0300

    Add 'Привет, мир!'

---
+++ src/java/ru/synergyitacademy/lesson1/Launcher.java
@@ -0,0 +1,9 @@
+package ru.synergyitacademy.lesson1;
+
+public class Launcher {
+
+    public static void main(String[] args) throws Exception {
+        String str = "Привет, мир!";
+        System.out.println(str);
+    }
+}
